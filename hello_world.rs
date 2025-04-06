958f6649c2af29344b784813f7e228691882c843
laithalkinani <alkinanl@uwindsor.ca>
2025-04-06 16:17:41 -0700

    wrote hello world

---
+++ hello_world.rs
@@ -0,0 +1,14 @@
+fn main () {
+    println!("Hello, World!"); //semi-colon terminated lines
+}
+
+/*
+Explanation:
+main is the entry point for any Rust program (like C)
+fn declares a function
+rust is an ahead of time compiled language, meaning you can run it and give the exe to someone else, and
+they don't even need to have rust installed 
+to run this on windows, type in the command terminal: 
+./hello_world
+
+*/
