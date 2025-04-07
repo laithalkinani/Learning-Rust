4c89b46c2b2a2f646f6f0206847bbac8538f1776
laithalkinani <alkinanl@uwindsor.ca>
2025-04-06 22:28:46 -0700

    figuring out why Cargo.toml isn't working

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,3 +1,26 @@
+//Build a guessing game. 
+
+use std::io; //io comes from std library
+
 fn main() {
-    println!("Hello, world!");
+    println!("Guess the number!");
+    println!("Please input your number guess.");
+
+    let mut guess = String::new();
+    //let creates the variable
+    //mut means we can expect the value to change at some point (more on this later)
+    //new is a function associated with String, std library
+    //so when we call new() we just create an empty string
+    //and we assign it to guess, a mutable variable
+
+
+    io::stdin() //handle standard input to terminal
+        .read_line(&mut guess) //no semi colon cause command not done
+        .expect("Failed to read line!"); 
+        //references are immutable by default
+        //so we pass &mut guess to tell the compiler to pass guess as a mutable reference
+        //the newline and tab is for readability, we could've done read_line(blah).expect("blah") 
+        //read_line returns Return, an enum with Ok and Err members
+        //instance of Return has method expect which, if Return->Err, gets called and crashes the program
+    println!("You guessed: {}", guess);
 }
---
+++ guessing_game/target/debug/guessing_game.d
@@ -0,0 +1,1 @@
+C:\Users\HP\Learning-Rust\guessing_game\target\debug\guessing_game.exe: C:\Users\HP\Learning-Rust\guessing_game\src\main.rs
